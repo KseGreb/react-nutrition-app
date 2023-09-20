45b106195cf406cc9a8b4678d4c010bd6ff03e68
“KseGreb” <ksegreb@gmail.com>
2023-09-20 11:39:03 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+
+import { useEffect } from 'react';
 import './App.css';
 
+
+
 function App() {
+
+  const API_ID = "e6550d1b";
+  const API_KEY = "4fe3203573bce172fbc3364a88b4293f";
+  const API_URL = "https://api.edamam.com/api/nutrition-details";
+  //https://api.edamam.com/api/nutrition-details?app_id=e6550d1b&app_key=4fe3203573bce172fbc3364a88b4293f
+
+
+  
+  useEffect(()=>{
+    
+    const getNutrition = async () => {
+  
+      const response = await fetch(`${API_URL}?app_id=${API_ID}&app_key=${API_KEY}`, 
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          },
+          body: JSON.stringify()
+        });
+    
+    const data = await response.json();
+    console.log(data)
+  }
+    getNutrition();
+    
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>nutrition</p>
     </div>
   );
 }
